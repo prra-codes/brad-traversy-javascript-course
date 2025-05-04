fbae4c711b7209ef3aa8d2169e878f7bf89ec414
peter <peter.arth5@gmail.com>
2025-05-04 11:04:17 +0100

    Completed Section 2 Arrays and Objects

---
+++ 02-arrays-and-objects/03-nesting-concat-spread/script.js
@@ -0,0 +1,41 @@
+let x;
+
+const fruits = ['apple', 'pear', 'orange'];
+const berries = ['strawberry', 'blueberry', 'rasberry'];
+
+// fruits.push(berries);
+
+// x = fruits[3][1];
+
+const allFruits = [fruits, berries];
+
+x = allFruits[1][0];
+
+x = fruits.concat(berries);
+
+// Spread Operator (...)
+
+x = [...fruits, ...berries];
+
+console.log(x);
+
+// Flatten Arrays
+const arr = [1, 2, [3, 4], 5, [6, 7], 8];
+
+// console.log(arr);
+
+x = arr.flat();
+
+// Static Methods on Array Object
+
+x = Array.isArray('Hello');
+
+x = Array.from('12345');
+
+const a = 1;
+const b = 2;
+const c = 3;
+
+x = Array.of(a, b, c);
+
+console.log(x);
