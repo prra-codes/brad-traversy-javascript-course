d4490bdbe8fb6c6685a09340b5be8662e7b04da1
peter <peter.arth5@gmail.com>
2025-05-06 10:23:11 +0100

    Completed Section 4 Logic and Control Flow

---
+++ 04-logic-control-flow/05-truthy-falsy/script.js
@@ -0,0 +1,64 @@
+// Falsy Values:
+// - false
+// - 0
+// - "" or '' (Empty string)
+// - null
+// - undefined
+// - NaN
+
+// Anything that is not falsy will be truthy
+
+// Truthy Values:
+// - Everything else that is not falsy
+// - true
+// - '0' (0 in a string)
+// - ' ' (space in a string)
+// - 'false' (false in a string)
+// - [] (empty array)
+// - {} (empty object)
+// - function () {} (empty function)
+
+const x = function () {};
+
+if (x) {
+  console.log('This is truthy');
+} else {
+  console.log('This is falsy');
+}
+
+console.log(Boolean(x));
+
+// Truthy and falsy caveats
+
+const children = 0;
+
+if (!isNaN(children)) {
+  console.log(`You have ${children} children`);
+} else {
+  console.log('Please enter number of children');
+}
+
+// Checking for empty arrays
+const posts = ['Post One'];
+
+if (posts.length > 0) {
+  console.log('List Posts');
+} else {
+  console.log('No Posts To List');
+}
+
+// Checking for empty objects
+const user = {
+  name: 'Brad',
+};
+
+if (Object.keys(user).length > 0) {
+  console.log('List user');
+} else {
+  console.log('No User');
+}
+
+// Loose Equality (==)
+console.log(false === 0);
+console.log('' === 0);
+console.log(null === undefined);
