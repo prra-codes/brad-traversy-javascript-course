34b1f9b75889a2eee872cbd8f9177e053018661b
peter <peter.arth5@gmail.com>
2025-05-07 10:22:57 +0100

    gone through more of Section 5, Loops, Iteration & Array Methods

---
+++ 05-iteration-array-methods/07-forEach/script.js
@@ -0,0 +1,26 @@
+// Any function that is passed as an argument to another function so that it can be executed in that other function is called as a callback function.
+
+const socials = ['Twitter', 'LinkedIn', 'Facebook', 'Instagram'];
+
+// console.log(socials.__proto__);
+
+// socials.forEach(function (item) {
+//   console.log(item);
+// });
+
+// socials.forEach((item, index, arr) => console.log(`${index} - ${item}`, arr));
+
+function logSocials(social) {
+  console.log(social);
+}
+
+// socials.forEach(logSocials);
+
+const socialObjs = [
+  { name: 'Twitter', url: 'https://twitter.com' },
+  { name: 'Facebook', url: 'https://facebook.com' },
+  { name: 'Linkedin', url: 'https://linkedin.com' },
+  { name: 'Instagram', url: 'https://instagram.com' },
+];
+
+socialObjs.forEach((item) => console.log(item.url));
