266afdf661c74be174a8ebd73c87820d89f93634
peter <peter.arth5@gmail.com>
2025-05-08 08:44:31 +0100

    Done array filter part

---
+++ 05-iteration-array-methods/08-filter/script.js
@@ -0,0 +1,18 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+// const evenNumbers = numbers.filter(function (number) {
+//   return number % 2 === 0;
+// });
+
+// Short version
+// const evenNumbers = numbers.filter((number) => number % 2 === 0);
+
+// Same with forEach
+// const evenNumbers = [];
+// numbers.forEach((number) => {
+//   if (number % 2 === 0) {
+//     evenNumbers.push(number);
+//   }
+// });
+
+console.log(evenNumbers);
